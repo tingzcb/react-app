7b6288f41c1ee32bfda3428462fb70ccd8c2b5bb
tingzcb <tingting.sun@cqumail.com>
2023-06-03 13:32:03 +1000

    add Boootstrap library and ListGroup component with Fragment

--- src/Message.tsx
+++ src/Message.tsx
@@ -1,8 +1,18 @@
+import { Fragment } from "react";
 
-function Message(){
-    return <h1>Hello Tina!</h1>;
-
-
+function Message() {
+  return (
+    <Fragment>
+      <h1>List</h1>
+      <ul className="list-group">
+        <li className="list-group-item">An item</li>
+        <li className="list-group-item">A second item</li>
+        <li className="list-group-item">A third item</li>
+        <li className="list-group-item">A fourth item</li>
+        <li className="list-group-item">And a fifth one</li>
+      </ul>
+    </Fragment>
+  );
 }
 
-export default Message; 
+export default Message;
---
+++ src/components/ListGroup.tsx
@@ -0,0 +1,7 @@
+import Message from "../Message";
+
+function ListGroup(){
+    return <div><Message/></div>;
+}
+
+export default ListGroup;
